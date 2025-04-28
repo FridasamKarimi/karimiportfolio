5dd74c86f4338fb7379dbcd098ce7c436974fb38
FridasamKarimi <muriithifridasam@gmail.com>
2025-04-28 10:28:44 +0300

    new js

---
+++ js/script.js
@@ -0,0 +1,20 @@
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+      navbar.classList.add('scrolled');
+    } else {
+      navbar.classList.remove('scrolled');
+    }
+  });
+  
+  document.querySelectorAll('.nav-link').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+      const targetId = this.getAttribute('href').substring(1);
+      const targetElement = document.getElementById(targetId);
+      window.scrollTo({
+        top: targetElement.offsetTop - 70, // Adjust for navbar height
+        behavior: 'smooth'
+      });
+    });
+  });
